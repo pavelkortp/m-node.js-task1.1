4c14df58fcd44548db883b5183964fccbe9fce2e
Pavlo <pavelkortp@gmail.com>
2023-10-06 02:13:55 +0300

    some progress with regex (validatePass ready mb)

--- com/shpp/pzhurbytskyi/regEx.js
+++ com/shpp/pzhurbytskyi/regEx.js
@@ -1,13 +1,13 @@
 function Validator() {
 
-    // /**
-    //  * Validates an email.
-    //  * @param {string} string email.
-    //  */
-    // this.validateEmail = (string) => {
+    /**
+     * Validates an email.
+     * @param {string} string email.
+     */
+    this.validateEmail = (string) => {
 
-    //     return string.match(/^(?![.-+])[\w+.+-]{2,20}@[\w+.!$%&*+/=?^_-]{1,15}.[\w+{1,5}]/) != null;
-    // }
+        return string.match(/(?!-)[-\.\+\w+]{2,20}@[.!$%&’*/=?^_+\w+]{1,15}\.[A-Za-z]{1,5}/g) != null;
+    }
 
     /**
      * Validates a phone number.
@@ -22,23 +22,28 @@ function Validator() {
      * @param {string} string password. 
      */
     this.validatePassword = (string) => {
-        return string.match(/[\w+]{8}/) != null;
+        return string.match(/(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*\w+.{7}/g) != null;
     }
 
 }
 
 const v = new Validator();
-// console.log(v.validateEmail('fi@secondpart.end'));
-// console.log(v.validateEmail('first-part@.se=cond%p.art.end'));
-// console.log(v.validateEmail('fi@secondpart.end'));
+console.log(v.validateEmail('fi@secondpart.end'));
+console.log(v.validateEmail('first-part@.se=cond%p.art.end'));
+console.log(v.validateEmail('fi@secondpart.end'));
 
-// console.log(v.validateEmail('f@secondart.end,'));
-// console.log(v.validateEmail('first-part@.se=cond@part.end'));
-// console.log(v.validateEmail('-firstpart@.se=cond%.enddeded'));
-// console.log(v.validateEmail('firs_tpart@.se.en'));
-// console.log(v.validateEmail('firstpart@.se.enddeded'));
+console.log(v.validateEmail('f@secondart.end,'));
+console.log(v.validateEmail('first-part@.se=cond@part.end'));
+console.log(v.validateEmail('-firstpart@.se=cond%.enddeded'));
+console.log(v.validateEmail('+firstpart@.se=cond%.enddeded'));
+console.log(v.validateEmail('.firstpart@.se=cond%.enddeded'));
+console.log(v.validateEmail('firs_tpart@.se.en'));
+console.log(v.validateEmail('firstpart@.se.enddeded'));
 
-console.log(v.validatePassword('C00l_Pass'));
-console.log(v.validatePassword('SupperPas1'));
-console.log(v.validatePassword('Cool_pass'));
-console.log(v.validatePassword('C00l'));
+// console.log(`C00l_Pass: ${v.validatePassword('C00l_Pass')}`);//Valid
+// console.log(`SupperPas1: ${v.validatePassword('SupperPas1')}`);//Valid
+// console.log(`DDDDDDDDDDDDDDsaaaaaaaaaaaa___111: ${v.validatePassword('DDDDDDDDDDDDDDsaaaaaaaaaaaa___111')}`);//Valid
+// console.log(`PP11__aa: ${v.validatePassword('PP11__aa')}`);//Valid
+// console.log(`1aaaa2aP: ${v.validatePassword('1aaaa2aP')}`);//Valid
+// console.log(`Cool_pass: ${v.validatePassword('Cool_pass')}`);
+// console.log(`C00l: ${v.validatePassword('C00l')}`);
